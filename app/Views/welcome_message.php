a580bd5c5e712b9a84d854615faf69f4430d5740
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2022-10-04 05:12:49 +0000

    .yml file updated changing the upload dir.

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -208,12 +208,12 @@
             <li class="menu-toggle">
                 <button onclick="toggleMenu();">&#9776;</button>
             </li>
-            <li class="menu-item hidden"><a href="#">Home</a></li>
-            <li class="menu-item hidden"><a href="https://codeigniter4.github.io/userguide/" target="_blank">Docs</a>
+            <li class="menu-item hidden"><a href="#">Home 1</a></li>
+            <li class="menu-item hidden"><a href="https://codeigniter4.github.io/userguide/" target="_blank">Docs 2</a>
             </li>
-            <li class="menu-item hidden"><a href="https://forum.codeigniter.com/" target="_blank">Community</a></li>
+            <li class="menu-item hidden"><a href="https://forum.codeigniter.com/" target="_blank">Community 3</a></li>
             <li class="menu-item hidden"><a
-                    href="https://github.com/codeigniter4/CodeIgniter4/blob/develop/CONTRIBUTING.md" target="_blank">Contribute</a>
+                    href="https://github.com/codeigniter4/CodeIgniter4/blob/develop/CONTRIBUTING.md" target="_blank">Contribute 4</a>
             </li>
         </ul>
     </div>
